3a5b24c64eada753b9853f7ef242cb407671d1d8
Piotr Kaminski <piotr@reviewable.io>
2026-07-18 22:13:17 -0700

    Switch to Yarn and update dependencies.

--- grunt/config/execute.js
+++ grunt/config/execute.js
@@ -1,7 +1,24 @@
 'use strict';
 
+var childProcess = require('child_process');
+
 module.exports = {
   test: {
-    src: ['test/*.js']
+    call: function(grunt) {
+      grunt.file.expand('test/*.js').forEach(function(file) {
+        grunt.log.writeln('-> executing ' + file);
+
+        var result = childProcess.spawnSync(process.execPath, [file], {
+          stdio: 'inherit'
+        });
+
+        if (result.error) {
+          grunt.fail.warn(result.error);
+        }
+        if (result.status) {
+          grunt.fail.warn(file + ' exited with code ' + result.status);
+        }
+      });
+    }
   }
 };
